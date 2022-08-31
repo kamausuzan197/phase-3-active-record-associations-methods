ea7c70ae6dba73af47ffd22f1452a1cf7f23caf4
kamausuzan197 <kamaususan197@gmail.com>
2022-08-31 09:38:34 +0300

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,24 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    genres = []
+    self.songs.each do |song|
+      if genres.include?(song.genre)
+        return false
+      else
+        genres << song.genre
+      end
+    end
+    genres.count
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,29 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
+
+  def artist_array
+    artists = []
+    self.songs.each do |song|
+      if artists.include?(song.artist)
+        return false
+      else 
+        artists << song.artist
+      end
+    end
+    artists
+  end
+
 
   def artist_count
     # return the number of artists associated with the genre
+self.artist_array.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artist_array.map {|a| a.name}
   end
 end
